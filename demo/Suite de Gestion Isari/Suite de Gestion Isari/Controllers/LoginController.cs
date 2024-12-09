4e2491efe9314c447bb8359cbcf1159eb1df113b
andyma17 <101306238+andyma17@users.noreply.github.com>
2024-12-09 00:48:57 -0600

    Add files via upload
    Puesto y login controller

---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/LoginController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Suite_de_Gestion_Isari.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: LoginController
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // GET: LoginController/Details/5
+        public ActionResult OlvideContraseña()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/PuestoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Suite_de_Gestion_Isari.Controllers
+{
+    public class PuestoController : Controller
+    {
+        public IActionResult AgregarPuesto()
+        {
+            return View();
+        }
+        
+
+        public IActionResult ConsultaPuesto()
+        {
+            return View();
+        }
+
+        public IActionResult EditarPuesto()
+        {
+            return View();
+        }
+
+
+
+
+
+
+    }
+}
