0f6e4ebc087d151ec302da21e5278ad314d4ba27
andyma17 <101306238+andyma17@users.noreply.github.com>
2024-12-09 00:48:05 -0600

    Add files via upload
    Controlador Usuario

---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/UsuarioController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Suite_de_Gestion_Isari.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        public IActionResult Registro()
+        {
+            return View();
+        }
+        
+
+        public IActionResult ConsultaUsuario()
+        {
+            return View();
+        }
+
+
+        public IActionResult EditarUsuario()
+        {
+            return View();
+        }
+
+        public IActionResult CambioContraseña()
+        {
+            return View();
+        }
+        public IActionResult ActualizarPerfil()
+        {
+            return View();
+        }
+        
+
+
+    }
+}
