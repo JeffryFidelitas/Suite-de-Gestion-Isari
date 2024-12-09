2033754a362a689f967c04e20232ed8947e193b2
andyma17 <101306238+andyma17@users.noreply.github.com>
2024-12-09 00:58:00 -0600

    Add files via upload

---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/CategoriaController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Suite_de_Gestion_Isari.Controllers
+{
+    public class CategoriaController : Controller
+    {
+      
+        public ActionResult AgregarCategoria()
+        {
+            return View();
+        }
+
+        public ActionResult ConsultaCategoria()
+        {
+            return View();
+        }
+        public ActionResult EditarCategoria()
+        {
+            return View();
+        }
+
+
+        
+
+    }
+}
