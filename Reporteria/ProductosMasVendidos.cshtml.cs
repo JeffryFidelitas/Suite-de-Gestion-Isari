ef83c91f65f398fd5a87d743cb30e85692ae5936
FrancinniZ <113738269+FrancinniZ@users.noreply.github.com>
2024-12-09 21:59:12 -0600

    Reportería

---
+++ Reporteria/ProductosMasVendidos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Suite_de_Gestion_Isari.Views.Reporteria
+{
+    public class ProductosMasVendidosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Reporteria/ReporteVentasDiarias.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Suite_de_Gestion_Isari.Views.Reporteria
+{
+    public class ReporteVentasDiariasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
