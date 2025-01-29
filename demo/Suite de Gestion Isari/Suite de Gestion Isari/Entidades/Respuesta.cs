8280e50acea01af710a9f0cf37f3e480c5e15dca
andyma17 <101306238+andyma17@users.noreply.github.com>
2025-01-28 22:52:33 -0600

    Add files via upload

---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Entidades/Respuesta.cs
@@ -0,0 +1,9 @@
+﻿namespace Suite_de_Gestion_Isari.Entidades
+{
+    public class Respuesta
+    {
+        public int Codigo { get; set; }
+        public string Mensaje { get; set; } = string.Empty;
+        public object? Contenido { get; set; }
+    }
+}
