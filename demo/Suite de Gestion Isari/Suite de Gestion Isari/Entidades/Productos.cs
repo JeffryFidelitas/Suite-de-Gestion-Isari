1e7200762db154c5325c0102649b0080a1143604
Jeffry Samuel Eduarte Rojas <jeduarte30114@ufide.ac.cr>
2025-02-02 12:52:10 -0600

    Modelo y Entidad Producto Listos

---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Entidades/Productos.cs
@@ -0,0 +1,13 @@
+namespace Suite_de_Gestion_Isari.Entidades
+{
+    public class Productos
+    {
+        public int ID_PRODUCTO { get; set; }
+        public string NOMBRE { get; set; } = string.Empty;
+        public string DESCRIPCION { get; set; } = string.Empty;
+        public double PRECIO { get; set; }
+        public int CANTIDAD_DISPONIBLE { get; set; }
+        public int ID_CATEGORIA { get; set; }
+        public int ID_PROVEEDOR { get; set; }
+    }
+}
---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Models/ProductoModel.cs
@@ -0,0 +1,72 @@
+using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Suite_de_Gestion_Isari.Entidades;
+
+namespace Suite_de_Gestion_Isari.Models
+{
+    public class ProductoModel
+    {
+        private readonly IConfiguration _conf;
+
+        public ProductoModel (IConfiguration conf)
+        {
+            _conf = conf;
+        }
+
+        [HttpPost]
+        public Respuesta AgregarProducto(Productos model)
+        {
+            using (var context = new SqlConnection(_conf.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                var respuesta = new Respuesta();
+                var result = context.Execute("AgregarProducto", new { model.NOMBRE, model.DESCRIPCION, model.PRECIO, model.CANTIDAD_DISPONIBLE, model.ID_CATEGORIA, model.ID_PROVEEDOR});
+
+                if (result > 0)
+                {
+                    respuesta.Codigo = 0;
+                    respuesta.Mensaje = "Producto agregado exitosamente.";
+                }
+                else
+                {
+                    respuesta.Codigo = -1;
+                    respuesta.Mensaje = "Ya existe el producto, "+model.NOMBRE+". Vuelva a intentarlo";
+                }
+                return respuesta;
+            }
+        }
+
+        public List<Productos> ObtenerProductos()
+        {
+            using (var context = new SqlConnection(_conf.GetConnectionString("DefaultConnection")))
+            {
+                return context.Query<Productos>("ObtenerProductos").ToList();
+            }
+        }
+
+        public Productos ConsultaProducto(long ID_PRODUCTO)
+        {
+            using (var context = new SqlConnection(_conf.GetConnectionString("DefaultConnection")))
+            {
+                var producto = context.QueryFirstOrDefault<Productos>(
+                    "ConsultaProducto",
+                    new {ID_PRODUCTO}
+                );
+                return producto ?? new Productos();
+            }
+        }
+
+        public bool ActualizarProducto(Productos model)
+        {
+            using (var context = new SqlConnection(_conf.GetConnectionString("DefaultConnection")))
+            {
+                var filasAfectadas = context.Execute(
+                    "ActualizarProducto",
+                    new { model.NOMBRE, model.DESCRIPCION, model.PRECIO, model.CANTIDAD_DISPONIBLE, model.ID_CATEGORIA, model.ID_PROVEEDOR}
+                );
+
+                return filasAfectadas > 0;
+            }
+        }
+    }
+}
