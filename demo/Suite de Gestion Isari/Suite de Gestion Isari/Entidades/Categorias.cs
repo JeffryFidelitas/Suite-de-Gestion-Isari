a925cc203b0aef1f2bb5989c8ca28b668795f98b
Jeffry Samuel Eduarte Rojas <jeduarte30114@ufide.ac.cr>
2025-02-04 18:17:00 -0600

    Modificaciones a Producto y empezado el trabajo de Categoria

---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Entidades/Categorias.cs
@@ -0,0 +1,9 @@
+
+namespace Suite_de_Gestion_Isari.Entidades
+{
+    public class Categorias
+    {
+        public int ID_CATEGORIA { get; set; }
+        public string DESCRIPCION { get; set; } = string.Empty;
+    }
+}
--- demo/Suite de Gestion Isari/Suite de Gestion Isari/Models/CategoriaModel.cs
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Models/CategoriaModel.cs
@@ -5,64 +5,64 @@ using Suite_de_Gestion_Isari.Entidades;
 
 namespace Suite_de_Gestion_Isari.Models
 {
-    public class ProductoModel
+    public class CategoriaModel
     {
         private readonly IConfiguration _conf;
 
-        public ProductoModel (IConfiguration conf)
+        public CategoriaModel (IConfiguration conf)
         {
             _conf = conf;
         }
 
         [HttpPost]
-        public Respuesta AgregarProducto(Productos model)
+        public Respuesta AgregarCategoria(Categorias model)
         {
             using (var context = new SqlConnection(_conf.GetSection("ConnectionStrings:DefaultConnection").Value))
             {
                 var respuesta = new Respuesta();
-                var result = context.Execute("AgregarProducto", new { model.NOMBRE, model.DESCRIPCION, model.PROVEEDOR, model.PRECIO, model.CANTIDAD_DISPONIBLE, model.ID_CATEGORIA });
+                var result = context.Execute("AgregarCategoria", new { model.DESCRIPCION });
 
                 if (result > 0)
                 {
                     respuesta.Codigo = 0;
-                    respuesta.Mensaje = "Producto agregado exitosamente.";
+                    respuesta.Mensaje = "Categoria agregada exitosamente.";
                 }
                 else
                 {
                     respuesta.Codigo = -1;
-                    respuesta.Mensaje = "Ya existe el producto, "+model.NOMBRE+". Vuelva a intentarlo";
+                    respuesta.Mensaje = "Ya existe la categoria, "+model.DESCRIPCION+". Vuelva a intentarlo";
                 }
                 return respuesta;
             }
         }
 
-        public List<Productos> ObtenerProductos()
+        public List<Categorias> ObtenerCategorias()
         {
             using (var context = new SqlConnection(_conf.GetConnectionString("DefaultConnection")))
             {
-                return context.Query<Productos>("ObtenerProductos").ToList();
+                return context.Query<Categorias>("ObtenerCategorias").ToList();
             }
         }
 
-        public Productos ConsultaProducto(long ID_PRODUCTO)
+        public Categorias ConsultaCategoria(long ID_CATEGORIA)
         {
             using (var context = new SqlConnection(_conf.GetConnectionString("DefaultConnection")))
             {
-                var producto = context.QueryFirstOrDefault<Productos>(
-                    "ConsultaProducto",
-                    new {ID_PRODUCTO}
+                var categoria = context.QueryFirstOrDefault<Categorias>(
+                    "ConsultaCategoria",
+                    new {ID_CATEGORIA}
                 );
-                return producto ?? new Productos();
+                return categoria ?? new Categorias();
             }
         }
 
-        public bool ActualizarProducto(Productos model)
+        public bool ActualizarCategoria(Categorias model)
         {
             using (var context = new SqlConnection(_conf.GetConnectionString("DefaultConnection")))
             {
                 var filasAfectadas = context.Execute(
-                    "ActualizarProducto",
-                    new { model.ID_PRODUCTO, model.NOMBRE, model.DESCRIPCION, model.PROVEEDOR, model.PRECIO, model.CANTIDAD_DISPONIBLE, model.ID_CATEGORIA }
+                    "ActualizarCategoria",
+                    new { model.ID_CATEGORIA, model.DESCRIPCION }
                 );
 
                 return filasAfectadas > 0;
