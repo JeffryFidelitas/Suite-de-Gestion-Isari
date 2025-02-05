c49e34c8a4e067270d4ea38a71cc604f75bb8cf0
Jeffry Samuel Eduarte Rojas <jeduarte30114@ufide.ac.cr>
2025-02-04 18:33:54 -0600

    Agregadas acciones a controller, renombrado crear -> agregar

--- demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/InventarioController.cs
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/InventarioController.cs
@@ -1,11 +1,19 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Suite_de_Gestion_Isari.Entidades;
 using Suite_de_Gestion_Isari.Models;
 
 namespace Suite_de_Gestion_Isari.Controllers;
 
 public class InventarioController : Controller
 {
+    private readonly ProductoModel _productoModel;
+
+    public InventarioController(IConfiguration configuration)
+    {
+        _productoModel = new ProductoModel(configuration);
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -23,8 +31,30 @@ public class InventarioController : Controller
         return View();
     }
 
-    public IActionResult Crear()
+    [HttpGet]
+    public IActionResult Agregar()
     {
         return View();
+    }
+
+    [HttpPost]
+    public IActionResult Agregar(Productos model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var respuesta = _productoModel.AgregarProducto(model);
+        if (respuesta.Codigo == 0)
+        {
+            TempData["SuccessMessage"] = respuesta.Mensaje;
+            return RedirectToAction("Index");
+        }
+        else
+        {
+            ViewBag.ErrorMessage = respuesta.Mensaje;
+            return View(model);
+        }
     }
 }
