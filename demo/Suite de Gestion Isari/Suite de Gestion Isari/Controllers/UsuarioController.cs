c2016408ad5eb5d053abf61e9a777c6379a7300d
andyma17 <andymadrigal41@gmail.com>
2025-02-06 21:42:28 -0600

    Agregar y consultar Empleados

--- demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/UsuarioController.cs
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Controllers/UsuarioController.cs
@@ -1,18 +1,56 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Suite_de_Gestion_Isari.Entidades;
+using Suite_de_Gestion_Isari.Models;
 
 namespace Suite_de_Gestion_Isari.Controllers
 {
     public class UsuarioController : Controller
     {
-        public IActionResult Registro()
+
+        private readonly UsuarioModel _usuario;
+
+        public UsuarioController(IConfiguration configuration)
+        {
+            _usuario = new UsuarioModel(configuration);
+        }
+
+        [HttpGet]
+        public IActionResult Agregar_Empleado()
         {
             return View();
         }
-        
 
-        public IActionResult Index()
+
+        [HttpPost]
+        public IActionResult Agregar_Empleado(Empleado model)
         {
-            return View();
+            if (!ModelState.IsValid)
+            {
+
+                return View(model);
+            }
+
+            var respuesta = _usuario.AgregarEmpleado(model);
+
+            if (respuesta.Codigo == 0)
+            {
+
+                TempData["SuccessMessage"] = respuesta.Mensaje;
+                return RedirectToAction("ConsultarEmpleados");
+            }
+            else
+            {
+
+                ViewBag.ErrorMessage = respuesta.Mensaje;
+                return View(model);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult ConsultarEmpleados()
+        {
+            var empleados = _usuario.ConsultarEmpleados();
+            return View(empleados);
         }
 
 
