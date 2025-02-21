1c9cfb629902a7b11d01b87b33c54408177ae2ab
FrancinniZ <fzuniga30280@ufide.ac.cr>
2025-02-20 21:09:00 -0600

    RECUPERACION CONTRASEÑA

---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Services/EmailService.cs
@@ -0,0 +1,39 @@
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using MimeKit;
+
+namespace Suite_de_Gestion_Isari.Services
+{
+    public class EmailService
+    {
+        public async Task<bool> EnviarCorreoRecuperacion(string destinatario, string nuevaContrasena)
+        {
+            try
+            {
+                var mensaje = new MimeMessage();
+                mensaje.From.Add(new MailboxAddress("Soporte", "francinnizi69@gmail.com"));
+                mensaje.To.Add(new MailboxAddress("", destinatario));
+                mensaje.Subject = "Recuperación de Contraseña";
+
+                mensaje.Body = new TextPart("plain")
+                {
+                    Text = $"Su nueva contraseña temporal es: {nuevaContrasena}\n\nPor favor, inicie sesión y cambie su contraseña."
+                };
+
+                using (var cliente = new SmtpClient())
+                {
+                    await cliente.ConnectAsync("smtp.gmail.com", 587, SecureSocketOptions.StartTls);
+                    await cliente.AuthenticateAsync("francinnizi69@gmail.com", "hzfrkrmfyosecrpv");
+                    await cliente.SendAsync(mensaje);
+                    await cliente.DisconnectAsync(true);
+                }
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ demo/Suite de Gestion Isari/Suite de Gestion Isari/Services/EmpleadoService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace Suite_de_Gestion_Isari.Services
+{
+    public class EmpleadoService
+    {
+        private readonly IConfiguration _conf;
+
+        public EmpleadoService(IConfiguration conf)
+        {
+            _conf = conf;
+
+        }
+        public async Task<string> RecuperarContrasena(string correo)
+        {
+            string nuevaContrasena = null;
+
+            using (var connection = new SqlConnection(_conf.GetSection("ConnectionStrings:DefaultConnection").Value))
+            {
+                await connection.OpenAsync();
+                using (var command = new SqlCommand("Recuperar_Contrasena", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddWithValue("@Email", correo);
+
+                    var result = await command.ExecuteScalarAsync();
+                    if (result != null)
+                    {
+                        nuevaContrasena = result.ToString();
+                    }
+                }
+            }
+
+            return nuevaContrasena;
+        }
+    }
+}
