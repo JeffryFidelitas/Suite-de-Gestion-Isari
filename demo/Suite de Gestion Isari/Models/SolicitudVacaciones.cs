bc8f7d2b97c50a0ce21a0bc7e5bc2c48f1bdf1dd
AlejandraLH0425 <alaura20259@ufide.ac.cr>
2025-03-10 19:00:53 -0600

    Create SolicitudVacaciones.cs

---
+++ demo/Suite de Gestion Isari/Models/SolicitudVacaciones.cs
@@ -0,0 +1,15 @@
+namespace Suite_de_Gestion_Isari.Entidades
+{
+    public class SolicitudVacaciones
+    {
+        public int ID_SOLICITUD { get; set; }
+        public int ID_EMPLEADO { get; set; }
+        public DateTime FECHA_INICIO { get; set; }
+        public DateTime FECHA_FIN { get; set; }
+        public int DIAS_SOLICITADOS { get; set; }
+        public string ESTADO { get; set; } = "Pendiente";
+        public DateTime FECHA_SOLICITUD { get; set; } = DateTime.Now;
+        public string MOTIVO { get; set; }
+    }
+
+}
